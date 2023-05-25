c79d88b96afa3a95487596a1cc3b3474c9ec1054
kellite <gyu9907@gmail.com>
2023-05-25 17:00:19 +0900

    merge suggest

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const postRouter = require('./routes/posting');
 const reportRouter = require('./routes/report');
 const recommendRouter = require('./routes/recommend');
 const locationRouter = require('./routes/location');
+const suggestRouter = require('./routes/suggest');
 
 //https 모듈
 const https = require('https');
@@ -27,6 +28,7 @@ app.use('/posting', postRouter);
 app.use('/report', reportRouter);
 app.use('/recommend', recommendRouter);
 app.use('/location', locationRouter);
+app.use('/suggest', suggest1Router);
 
 // 실제 서버 구동 여부 true: 서버, false: 로컬
 const prod = false;
---
+++ routes/suggest.js
@@ -0,0 +1,180 @@
+const express = require('express');
+const db = require("../db"); // 데이터베이스
+const router = express.Router();
+const picture_url = 'https://www.picplace.kro.kr/posting/picture/';
+
+// 데이터 베이스 연결
+db.connect(function(err) {
+    if(err) {
+        console.log('Unable to connect to MySQL');
+        process.exit(1);
+    }
+});
+
+function dateFormat(date) {
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+
+    month = month >= 10 ? month : '0' + month;
+    day = day >= 10 ? day : '0' + day;
+    hour = hour >= 10 ? hour : '0' + hour;
+
+    return date.getFullYear() + '-' + month + '-' + day;
+}
+
+router.get('/week-posting', (req, res) => {
+    let today = new Date().setDate(new Date().getDate() + 1);
+    let lastweek = new Date().setDate(new Date().getDate() - 7);
+
+    today = dateFormat(new Date(today));
+    lastweek = dateFormat(new Date(lastweek));
+
+    const sql = 'SELECT * FROM posting WHERE postdate BETWEEN ? AND ? AND disclosure != "비공개"';
+    db.get().query(sql, [lastweek, today], (err, postingresults) => {
+        if (err) {
+            console.error(err);
+            const error = { "errorCode": "U009", "message": "데이터베이스에 접속하지 못했습니다." };
+            res.status(500).json(error);
+        } else if (postingresults.length > 0) {
+            const getElCount = (arr) => arr.reduce((accu, curr) => { 
+                    accu[curr] = (accu[curr] || 0)+1; 
+                    return accu;
+                }, {});
+            const modeKey = newObject => Object.keys(newObject).reduce((acc, cur) =>
+                    newObject[acc] > newObject[cur] ? acc : cur
+                );
+            
+            const postingRoads = postingresults.map(postingresult => postingresult.roadname);
+            const mostPostingRoad = modeKey(getElCount(postingRoads));
+
+            const results = postingresults.filter((postingresult) => {
+                    return (postingresult.roadname === mostPostingRoad);
+                });
+
+            const postingIds = results.map(postingresult => postingresult.postingid);
+            const sql = 'SELECT * FROM picture WHERE postingid IN ?';
+            const sqlParams = [postingIds];
+
+            db.get().query(sql, [sqlParams], (err, pictureresults) => {
+                if (err) {
+                    console.error(err);
+                    const error = { "errorCode": "U009", "message": "데이터베이스에 접속하지 못했습니다." };
+                    res.status(500).json(error);
+                } else if (pictureresults.length > 0) {
+                    for (let i = 0; i < results.length; i++) {
+                        let picarr = new Array();
+                        pictureresults.forEach(function (picture) {
+                            if (results[i].postingid == picture.postingid) {
+                                picarr.push(picture_url + picture.pictureid);
+                            }
+                        });
+                        results[i].pictures = picarr;
+                    }
+
+                    // 현재 게시글의 추천 수를 가져옵니다.
+                    const countSql = 'SELECT postingid, COUNT(*) AS count FROM recommand WHERE postingid IN ? GROUP BY postingid;';
+                    const countParams = [postingIds];
+                    db.get().query(countSql, [countParams], (err, result) => {
+                        if (err) {
+                            console.error(err);
+                            const error = { "errorCode": "U015", "message": "추천 수를 가져오는 동안 오류가 발생했습니다." };
+                            return res.status(500).json(error);
+                        } 
+                        const counts = result.reduce((acc, row) => {
+                            acc[row.postingid] = row.count;
+                            return acc;
+                        }, {});
+
+                        results.forEach(posting => {
+                            posting.recommendCount = counts[posting.postingid] || 0;
+                        });
+                    
+                        res.json(results);
+                    });
+                } else {
+                    for (let i = 0; i < results.length; i++) {
+                        results[i].pictures = "";
+                    }
+                    res.json(results);
+                }
+            });
+        } else {
+            const error = { "errorCode": "U010", "message": "DB 검색 결과가 없습니다." };
+            res.status(400).json(error);
+        }
+    });
+});
+
+router.get('/', (req, res) => {
+    const sql = `
+        SELECT p.postingid, p.disclosure, p.content, p.roadname, p.userid, p.postdate,
+             GROUP_CONCAT(DISTINCT CONCAT('${picture_url}', pi.pictureid)) AS pictures,
+             GROUP_CONCAT(DISTINCT t.tag) AS tags,
+             COUNT(r.postingid) AS recommendCount
+        FROM posting p
+        LEFT JOIN picture pi ON p.postingid = pi.postingid
+        LEFT JOIN tags t ON p.postingid = t.postingid
+        LEFT JOIN recommand r ON p.postingid = r.postingid
+        WHERE t.tag = (
+            SELECT tag
+            FROM tags
+            ORDER BY RAND()
+            LIMIT 1
+        ) AND p.disclosure != '비공개'
+        GROUP BY p.postingid;
+    `;
+  
+    db.get().query(sql, (err, results) => {
+        if (err) {
+            console.error(err);
+            const error = { "errorCode": "U009", "message": "데이터베이스에 접속하지 못했습니다." };
+            res.status(500).json(error);
+        } else {
+            const response = results.map(result => {
+            const posting = {
+                    postingid: result.postingid,
+                    disclosure: result.disclosure,
+                    content: result.content,
+                    roadname: result.roadname,
+                    userid: result.userid,
+                    postdate: result.postdate,
+                    pictures: result.pictures ? result.pictures.split(',') : [],
+                    tags: result.tags ? result.tags.split(',') : [],
+                    recommendCount: result.recommendCount || 0
+                };
+                return posting;
+            });
+  
+            res.json(response);
+        }
+    });
+});
+
+router.get('/popular', (req, res) => {
+    const sql = `
+        SELECT p.*, COUNT(r.postingid) AS recommendCount
+        FROM posting p
+        LEFT JOIN recommand r ON p.postingid = r.postingid
+        WHERE p.roadname = (
+        SELECT roadname
+        FROM posting
+        GROUP BY roadname
+        ORDER BY COUNT(*) DESC
+        LIMIT 1
+        )
+        GROUP BY p.postingid;
+    `;
+
+    db.get().query(sql, (err, results) => {
+        if (err) {
+            console.error(err);
+            const error = { "errorCode": "U009", "message": "데이터베이스에 접속하지 못했습니다." };
+            return res.status(500).json(error);
+        }
+
+        res.json(results);
+    });
+});
+
+module.exports = router;
