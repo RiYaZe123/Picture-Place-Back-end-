04dc519490e0e37f5d046be07131426833b3baff
Sang Hyuk Hong <tkdgur3316@gmail.com>
2023-05-25 17:57:33 +0900

    change

--- routes/suggest.js
+++ routes/suggest.js
@@ -107,56 +107,54 @@ router.get('/week-posting', (req, res) => {
 });
 
 router.get('/random', (req, res) => {
-  const sql = `
-    SELECT p.postingid, p.disclosure, p.content, p.locationname, p.userid, p.postdate,
-         GROUP_CONCAT(DISTINCT CONCAT('${picture_url}', pi.pictureid)) AS pictures,
-         GROUP_CONCAT(DISTINCT t.tag) AS tags,
-         COUNT(r.postingid) AS recommendCount
-    FROM posting p
-    LEFT JOIN picture pi ON p.postingid = pi.postingid
-    LEFT JOIN tags t ON p.postingid = t.postingid
-    LEFT JOIN recommand r ON p.postingid = r.postingid
-    WHERE p.postingid IN (
-        SELECT t.postingid
-        FROM tags t
-        WHERE t.tag IN (
-            SELECT tag
-            FROM tags
-            ORDER BY RAND()
-            LIMIT 1
-        )
-    ) AND p.disclosure != '비공개'
-    GROUP BY p.postingid, p.disclosure, p.content, p.locationname, p.userid, p.postdate;
-  `;
-
-  db.get().query(sql, (err, results) => {
-    if (err) {
-      console.error(err);
-      const error = { "errorCode": "U009", "message": "데이터베이스에 접속하지 못했습니다." };
-      res.status(500).json(error);
-    } else {
-      const response = results.map(result => {
-        const posting = {
-          postingid: result.postingid,
-          disclosure: result.disclosure,
-          content: result.content,
-          locationname: result.locationname,
-          userid: result.userid,
-          postdate: result.postdate,
-          pictures: result.pictures ? result.pictures.split(',') : [],
-          tags: result.tags ? result.tags.split(',') : [],
-          recommendCount: result.recommendCount || 0
-        };
-        return posting;
-      });
-
-      res.json(response);
-    }
-  });
+    const sql = `
+      SELECT p.postingid, p.disclosure, p.content, p.locationname, p.userid, p.postdate,
+           GROUP_CONCAT(DISTINCT CONCAT('${picture_url}', pi.pictureid)) AS pictures,
+           (SELECT GROUP_CONCAT(DISTINCT t.tag) FROM tags t WHERE t.postingid = p.postingid) AS tags,
+           COUNT(r.postingid) AS recommendCount
+      FROM posting p
+      LEFT JOIN picture pi ON p.postingid = pi.postingid
+      LEFT JOIN recommand r ON p.postingid = r.postingid
+      WHERE p.postingid IN (
+          SELECT t.postingid
+          FROM tags t
+          WHERE t.tag = (
+              SELECT tag
+              FROM tags
+              WHERE postingid = p.postingid
+              ORDER BY RAND()
+              LIMIT 1
+          )
+      ) AND p.disclosure != '비공개'
+      GROUP BY p.postingid;
+    `;
+  
+    db.get().query(sql, (err, results) => {
+      if (err) {
+        console.error(err);
+        const error = { "errorCode": "U009", "message": "데이터베이스에 접속하지 못했습니다." };
+        res.status(500).json(error);
+      } else {
+        const response = results.map(result => {
+          const posting = {
+            postingid: result.postingid,
+            disclosure: result.disclosure,
+            content: result.content,
+            locationname: result.locationname,
+            userid: result.userid,
+            postdate: result.postdate,
+            pictures: result.pictures ? result.pictures.split(',') : [],
+            tags: result.tags ? result.tags.split(',') : [],
+            recommendCount: result.recommendCount || 0
+          };
+          return posting;
+        });
+  
+        res.json(response);
+      }
+    });
 });
-
-
-
+  
 router.get('/popular', (req, res) => {
     const sql = `
         SELECT p.*, COUNT(r.postingid) AS recommendCount
